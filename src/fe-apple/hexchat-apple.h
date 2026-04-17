bd24f4eba73917d8929d5cc84e23965419babf17
Richard Drake <rdrake@rdrake.org>
2026-04-17 17:49:17 -0400

    apple-shell: export pod adapter public header

--- src/fe-apple/hexchat-apple.h
+++ src/fe-apple/hexchat-apple.h
@@ -2,29 +2,6 @@
 
 #include <glib.h>
 
-typedef struct
-{
-	const char *config_dir;
-	int no_auto;
-	int skip_plugins;
-} hc_apple_runtime_config;
+#include "hexchat-apple-public.h"
 
-typedef enum
-{
-	HC_APPLE_EVENT_LOG_LINE = 0,
-	HC_APPLE_EVENT_LIFECYCLE = 1,
-} hc_apple_event_kind;
-
-typedef struct
-{
-	hc_apple_event_kind kind;
-	const char *text;
-} hc_apple_event;
-
-typedef void (*hc_apple_event_cb) (const hc_apple_event *event, void *userdata);
-
-gboolean hc_apple_runtime_start (const hc_apple_runtime_config *config,
-                                 hc_apple_event_cb callback,
-                                 void *userdata);
-gboolean hc_apple_runtime_post_command (const char *command);
-void hc_apple_runtime_stop (void);
+/* internal-only declarations continue below */
